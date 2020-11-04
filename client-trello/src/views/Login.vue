c408c7af06fcf727e9bafa8b885b927eaac6a6cc
javidRasouli <javid99.rasouli@gmail.com>
2020-11-04 16:31:23 +0330

    create 90% login page

--- client-trello/src/views/Login.vue
+++ client-trello/src/views/Login.vue
@@ -1,8 +1,13 @@
 <template>
-<div class="main-login min-h-full">
-  <div class="h-screen">
-    <form >
-      <input type="text" placeholder="username">
+<div class="min-h-full">
+  <div class="grid grid-rows-1 justify-items-center">
+    <img src="../assets/pic/farawin-nav.png" width="150" alt="" class="my-8">
+  </div>
+  <div class="grid grid-rows-1 justify-items-center md:h-full">
+    <form class="mt-6 md:mt-0 w-1/2 sm:w-2/5 md:w-1/3 lg:w-1/4">
+      <input type="text" placeholder="username" class="bolck w-full p-3 bg-transparent border rounded-3xl outline-none  focus:shadow-outline focus:border-blue-300">
+      <input type="password" placeholder="password" class="block my-6 w-full p-3 bg-transparent border rounded-3xl outline-none  focus:shadow-outline focus:border-blue-300">
+      <button type="submit" class="block my-6 w-full p-3 bg-red-700 border text-white bg-opacity-75 rounded-3xl focus:outline-none focus:disabled">Log in</button>
     </form>
   </div>
 </div>
@@ -19,12 +24,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style lang="scss">
-.main-login{
-  background-image:linear-gradient(rgba(22,22,22,0.3)0%,rgba(22,22,22,0.7)75%,#161616 100%),url('../assets/pic/login-back.jpg');
-      background-position: center;
-      background-repeat: no-repeat;
-      background-size: cover;
-}
-</style>
