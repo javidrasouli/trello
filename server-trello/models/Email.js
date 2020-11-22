1b12239150aae416729afcce56d891b0db24b612
javidrasouli <javid99.rasouli@gmail.com>
2020-11-22 09:22:29 +0330

    start create option

---
+++ server-trello/models/Email.js
@@ -0,0 +1,41 @@
+'use strict'
+const { ObjectId } = require('mongodb')
+const { FindAll, InsertOne, FindOne, UpdateOne } = require('../DB/crud')
+const { findperson } = require('./User')
+function check (pm) {
+  if (!pm || pm == '') {
+    return { success: false, status: 400, error: 'your message is empty' }
+  }
+}
+const send = async (message, boardID, token) => {
+  check(message)
+  const user = await findperson(token)
+  const pm = { message: message, boardID: ObjectId(boardID), person: user.username }
+  const ress = await InsertOne('message', pm)
+  return ress
+}
+
+const receivedAllpm = async (token) => {
+  const user = await findperson(token)
+  const teamsUser = await FindAll('boardTeam', {person: user.username})
+  let userMessage = []
+  for await (const team of teamsUser) {
+    const board = await FindOne('boards', {_id: team.boardID})
+    const messageBoard = await FindAll('message', { boardID: team.boardID })
+    userMessage.push({ board: board.name, count: messageBoard.length })
+  }
+  const see = await FindAll('see', {person: user.username})
+  return userMessage
+}
+
+const receivedBoardPm = async (boardID, token) => {
+  const pm = await FindAll('meassage', {boardID: ObjectId(boardID)})
+  const user = await findperson(token)
+  const see = await FindAll('see', {person: user.username})
+  for (const boardPm of see) {
+      if (boardPm.boardID == boardID) {
+        await UpdateOne('see', {_id: boardPm._id}, {count: pm.length})
+      }
+  }
+  return pm
+}
